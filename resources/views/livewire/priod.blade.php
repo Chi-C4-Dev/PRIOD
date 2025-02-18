ea4b3f681fb3b1864dbe370a4d9eb102536e4c18
mesack <chinhalamesack@gmail.com>
2025-02-18 13:57:28 +0100

    adicionado o cnd do bootstrap para tentar corrigir o css nao loaded

--- resources/views/livewire/priod.blade.php
+++ resources/views/livewire/priod.blade.php
@@ -12,7 +12,7 @@
    
   
 
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
        <!-- CSS do AOS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.css">
@@ -155,7 +155,7 @@
 </script>
 
     
-    <script src={{ asset('build/assets/js/jquery.min.js')}}></script>
+    <script src={{ asset('build/assets/js/jquery.min.js') }}></script>
     <script src={{ asset('build/assets/js/jquery-migrate-3.0.1.min.js')}}></script>
     <script src={{ asset('build/assets/js/jquery.stellar.min.js ')}}></script>
     <script src={{ asset('build/assets/js/owl.carousel.min.js')}}></script>
@@ -166,9 +166,10 @@
     <script src={{ asset('build/assets/js/bootstrap.min.js')}}></script>
     <script src={{ asset('build/assets/js/bootstrap.bundle.js')}}></script>
     
-    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         
 
