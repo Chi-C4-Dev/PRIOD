58ad6f68b2e32cd4e6b3239af4c84d780aa401e6
mesack <chinhalamesack@gmail.com>
2025-02-19 12:23:39 +0100

    adicionado o CDN do Tailwinds e comentado o @vite

--- resources/views/livewire/priod.blade.php
+++ resources/views/livewire/priod.blade.php
@@ -15,7 +15,6 @@
 <link rel="stylesheet" href="https://priod.up.railway.app/build/assets/css/owl.theme.default.min.css">
 <link rel="stylesheet" href="https://priod.up.railway.app/build/assets/css/animate.css">
 <link rel="stylesheet" href="https://priod.up.railway.app/build/assets/css/bootstrap.min.css">
-<script src="https://cdn.tailwindcss.com"></script>
 
 
    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
