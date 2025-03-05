fc9dd0cda361101d5f96e6f14e1e9cf4dc185f5a
unknown <chinhalamesack@gmail.com>
2025-03-05 22:19:01 +0100

    :test_tube: test(TesteCurso): alterado o conteudo no html 3

--- app/Livewire/TestCurso.php
+++ app/Livewire/TestCurso.php
@@ -7,14 +7,34 @@ use Livewire\Component;
 
 class TestCurso extends Component
 {
+     public $count = 1;
      public $categorias;
  
 
     public function render()
     {
         
+          // Carrega os cursos ao iniciar
+          $this->refreshCursos();
         return view('livewire.test-curso');
     }
 
+    
+public function increment()
+{
+    $this->count++;
+}
 
+public function decrement()
+{
+    $this->count--;
+}
+
+public function refreshCursos()
+{
+            
+   // Recupera categorias com seus serviços associados
+  $this-> categorias = categoria::all();
+
+}
 }
